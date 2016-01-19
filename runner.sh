9e52f686d79795997c657b18a84ccd399ef8a835
Nyeng <nett-tv-test@nrk.no>
2016-01-19 10:07:36 +0100

    runner.sh

--- runner.sh
+++ runner.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+
+#set
+#set -x
 TODAY=$(date)
 HOST=$(hostname)
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
+
 echo "-----------------------------------------------------"
 echo "Date: $TODAY                     Host:$HOST"
 echo "-----------------------------------------------------"
-#Run features
-"cucumber"
+cd /home/n07028/AppleCapy/features_appletv/
+echo "You're currently in directory"
+pwd
+bundle install
+cucumber
